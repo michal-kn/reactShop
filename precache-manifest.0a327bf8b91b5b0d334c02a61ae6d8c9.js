afecd5b437d8d142823d81b24b0b6d99bbaeb0b9
Michal Knapczyk <michalkna01@gmail.com>
2020-09-08 23:12:41 +0200

    Updates

---
+++ precache-manifest.0a327bf8b91b5b0d334c02a61ae6d8c9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f044fc3adafaac960551e8b9aa3db1",
+    "url": "/reactShop/index.html"
+  },
+  {
+    "revision": "c1ad66c94f413cc1119f",
+    "url": "/reactShop/static/css/main.bc077a93.chunk.css"
+  },
+  {
+    "revision": "ff0d32d118a294ce7ff6",
+    "url": "/reactShop/static/js/2.2e0e0e90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactShop/static/js/2.2e0e0e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ad66c94f413cc1119f",
+    "url": "/reactShop/static/js/main.1adabd2e.chunk.js"
+  },
+  {
+    "revision": "89ba3b80ab5113f20908",
+    "url": "/reactShop/static/js/runtime-main.c3cdec72.js"
+  },
+  {
+    "revision": "f7c541232ddf7d75d9425d4c5f78c8c3",
+    "url": "/reactShop/static/media/chair1.f7c54123.jpg"
+  },
+  {
+    "revision": "f9439649731347cff0b5f02c76e27354",
+    "url": "/reactShop/static/media/chair2.f9439649.jpg"
+  },
+  {
+    "revision": "bb0e26a934def7c8bb2a87196aedc9a5",
+    "url": "/reactShop/static/media/chair3.bb0e26a9.jpg"
+  },
+  {
+    "revision": "9ce3866d95251eb49e45caf988d1376e",
+    "url": "/reactShop/static/media/chair4.9ce3866d.jpg"
+  },
+  {
+    "revision": "fad8674be4278fd927d0cd72de3eef10",
+    "url": "/reactShop/static/media/chair5.fad8674b.jpg"
+  },
+  {
+    "revision": "af7665f8b207c46ada0239eb0cb3cc73",
+    "url": "/reactShop/static/media/chair6.af7665f8.jpg"
+  },
+  {
+    "revision": "cf350719540005b5a9058606cab7d0aa",
+    "url": "/reactShop/static/media/shop_photo.cf350719.jpg"
+  }
+]);
